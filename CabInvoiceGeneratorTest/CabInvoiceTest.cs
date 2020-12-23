bb40198dcf382d94bbc83da11351db83da6efdf0
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-23 19:09:22 +0530

    [NIJAM] Add:Given Distance and Time The Invoice Generator Should Return TotalFare.

--- CabInvoiceGeneratorTest/CabInvoiceTest.cs
+++ CabInvoiceGeneratorTest/CabInvoiceTest.cs
@@ -5,11 +5,20 @@ namespace CabInvoiceGeneratorTest
     public class Tests
     {
         InvoiceGenerator invoiceGenerator = null;
-        [SetUp]
-        public void Setup()
+        
+        /// <summary>
+        /// Test Case_1 Given Distance and Time The Invoice Generator Should Return TotalFare.
+        /// </summary>
+        
+        [Test]
+        public void GivenDistanceAndTimeReturnTotalFare()
         {
+            invoiceGenerator = new InvoiceGenerator();
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
         }
-
-
     }
 }
