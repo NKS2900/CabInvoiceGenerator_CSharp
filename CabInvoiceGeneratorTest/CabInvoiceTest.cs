c2da41703f3ecc4454ed55d350d2fd69d53e9c62
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-23 19:29:44 +0530

    [NIJAM] Add:Invoice Summary For Multiple Rides.

--- CabInvoiceGeneratorTest/CabInvoiceTest.cs
+++ CabInvoiceGeneratorTest/CabInvoiceTest.cs
@@ -20,5 +20,18 @@ namespace CabInvoiceGeneratorTest
             double expected = 25;
             Assert.AreEqual(expected, fare);
         }
+
+        /// <summary>
+        /// Test Case_2 The InvoiceSummery Should Return Total Fare InvoiceSummary for multiple Rides.
+        /// </summary>
+        [Test]
+        public void GivenMultipleRides_UsingInvoiceSummary_ShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            Assert.AreEqual(expectedSummary, summary);
+        }
     }
 }
